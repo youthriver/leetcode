5e2fae3efcfb146bb17ebfed1bb7bccef77ae374
U-DESKTOP-6K3OCSV\Unipus <664163046@qq.com>
2023-06-27 17:52:24 +0800

    add .gitignore

--- python/linkedList/rerverseLinkedList.py
+++ python/linkedList/rerverseLinkedList.py
@@ -1,4 +1,9 @@
 
+import logging
+import os
+
+logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s', level=logging.DEBUG)
+
 # https://blog.csdn.net/u011608357/article/details/36933337
 
 class ListNode:
@@ -7,21 +12,42 @@ class ListNode:
         self.next = None
 
 def list2linkedlist():
-    head = ListNode(1);  # 测试代码
-    p1 = ListNode(2);  # 建立链表1->2->3->4->None;
-    p2 = ListNode(3);
-    p3 = ListNode(4);
-    head.next = p1;
-    p1.next = p2;
+    head = ListNode(1)  # 测试代码
+    p1 = ListNode(2)  # 建立链表1->2->3->4->None;
+    p2 = ListNode(3)
+    p3 = ListNode(4)
+    head.next = p1
+    p1.next = p2
+    p2.next = p3
+    return head
 
 
-def reverseLinkedList():
-    ori = [1, 2, 3]
+def reverseLinkedList(head):
+    prev = None
+    curr = head
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    return prev
 
+def reverseLinkedList_recursive(head):
+    if head == None or head.next == None:
+        return head
+    new_head = reverseLinkedList_recursive(head.next)
+    head.next.next = head
+    head.next = None
 
-
-    return
-    
+    return new_head
 
 if __name__ == '__main__':
-    reverseLinkedList()
+    head = list2linkedlist()
+    # while head:
+    #     logging.info(head.val)
+    #     head = head.next
+    # new = reverseLinkedList(head)
+    new = reverseLinkedList_recursive(head)
+    while new:
+        logging.info(new.val)
+        new = new.next
