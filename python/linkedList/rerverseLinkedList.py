fae07402d0b38a6a6cf42536da2242664f3b1df4
youthriver <664163046@qq.com>
2023-06-27 09:34:41 +0800

    init

---
+++ python/linkedList/rerverseLinkedList.py
@@ -0,0 +1,27 @@
+
+# https://blog.csdn.net/u011608357/article/details/36933337
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def list2linkedlist():
+    head = ListNode(1);  # 测试代码
+    p1 = ListNode(2);  # 建立链表1->2->3->4->None;
+    p2 = ListNode(3);
+    p3 = ListNode(4);
+    head.next = p1;
+    p1.next = p2;
+
+
+def reverseLinkedList():
+    ori = [1, 2, 3]
+
+
+
+    return
+    
+
+if __name__ == '__main__':
+    reverseLinkedList()
