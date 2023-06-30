9fcd07ab29a37f9fdc35fba0c0777c4fd76240d1
youthriver <664163046@qq.com>
2023-06-30 18:22:43 +0800

    'binaryTree'

--- python/binaryTree/preorder.py
+++ python/binaryTree/preorder.py
@@ -1,4 +1,27 @@
 
+class TreeNode():
+    def __int__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def createTree():
+    a = TreeNode(1)
+    b = TreeNode(2)
+    c = TreeNode(3)
+    d = TreeNode(4)
+    e = TreeNode(5)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+
+
+def list2tree(arr):
+    for item in arr:
+        temp = TreeNode(item)
+
+
 def demo():
     pass
 
