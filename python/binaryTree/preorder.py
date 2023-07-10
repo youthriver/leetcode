eff358f161e6a68848ccb537441a4cbf95b7afa9
youthriver <664163046@qq.com>
2023-07-10 17:13:38 +0800

    'preorder'

--- python/binaryTree/preorder.py
+++ python/binaryTree/preorder.py
@@ -1,6 +1,6 @@
 
-class TreeNode():
-    def __int__(self, value):
+class TreeNode:
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
@@ -22,12 +22,20 @@ def list2tree(arr):
     if num == 0:
         return None
     root = TreeNode(arr[0])
-    temp = root
+    queue = [root]
     for index, item in enumerate(arr):
-        if index * 2 + 1 < num:
-            temp.left = TreeNode(arr[index * 2 + 1])
-        if index * 2 + 2 < num:
-            temp.right = TreeNode(arr[index * 2 + 2])
+        if len(queue) == 0:
+            continue
+        temp = queue[0]
+        if index * 2 + 1 < num and arr[index * 2 + 1]:
+            left = TreeNode(arr[index * 2 + 1])
+            temp.left = left
+            queue.append(left)
+        if index * 2 + 2 < num and arr[index * 2 + 2]:
+            right = TreeNode(arr[index * 2 + 2])
+            temp.right = right
+            queue.append(right)
+        del queue[0]
     return root
 
 
@@ -44,8 +52,35 @@ def list2tree_recursive(arr):
     root = trans(0)
     return root
 
+def preorder_recursive(root):
+    result = []
+    def trans(node):
+        if node:
+            result.append(node.value)
+            trans(node.left)
+            trans(node.right)
+    trans(root)
+    return result
+
+def preorder(root):
+    result = []
+    stacks = [root]
+    while stacks:
+        temp = stacks[0]
+        if temp:
+            result.append(temp.value)
+            stacks.append(temp.left)
+            stacks.append(temp.right)
+        del stacks[0]
+    return result
+
 def demo():
-    pass
+    arr = [3,9,20,None,None,15,7]
+    # root = list2tree(arr)
+    root = list2tree_recursive(arr)
+
+    result = preorder(root)
+    print(result)
 
 if __name__ == '__main__':
     demo()
