06bf9ce9f8dd3a1b8dea051ba107be26625c6695
youthriver <664163046@qq.com>
2023-07-20 10:43:51 +0800

    update

--- python/greedy/distribute_candy.py
+++ python/greedy/distribute_candy.py
@@ -12,12 +12,31 @@ logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(leve
 # 数据范围: 1 <= n <= 100000, 1 <= a_i <= 1000
 # [1,1,2] -> 4=1+1+2
 # [1,1,1] -> 3=1+1+1
+# 先从左到右遍历, 得分多的糖果多, 再从右向左遍历, 得分多的糖果多
 
 def distribute_candy(arr):
-    return
+    temp = [1] * len(arr)
+    for index, item in enumerate(arr[1::]):
+        if item > arr[index - 1]:
+            temp[index] = temp[index - 1] + 1
+    index = len(arr) - 2
+    while index >= 0:
+        item = arr[index]
+        if index == len(arr) - 1:
+            continue
+        if item > arr[index + 1]:
+            temp[index] = temp[index + 1] + 1
+        index -= 1
+    min_value = min(temp)
+    if min_value < 1:
+        temp += 1 - min_value
+    result = sum(temp)
+    return result
 
 def demo():
-    arr = []
+    arr = [1, 1, 2]
+    arr = [1, 1, 1]
+    arr = [2, 1, 1]
     result = distribute_candy(arr)
     logging.info(result)
 
