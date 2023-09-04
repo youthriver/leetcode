1892e8987eda16aa55fbc697b3cdef280987097c
youthriver <664163046@qq.com>
2023-09-04 18:22:58 +0800

    'update'

--- python/linkedList/reverseLinkedList_fromMtoN.py
+++ python/linkedList/reverseLinkedList_fromMtoN.py
@@ -10,12 +10,58 @@ logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(leve
 # 示例1: 输入 {1, 2, 3, 4, 5}, 2, 4, 返回值{1, 4, 3, 2, 5}
 # 示例2: 输入 {5}, 1, 1, 返回值{5}
 
+class ListNode:
+    def __init__(self, value):
+        self.val = value
+        self.next = None
 
 def createLinkedList(arr):
-    return
+    head = ListNode(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        node = ListNode(arr[i])
+        curr.next = node
+        curr = node
+    return head
+
+def create2linkedlist():
+    head = ListNode(1)
+    p1 = ListNode(2)
+    p2 = ListNode(3)
+    p3 = ListNode(4)
+    head.next = p1
+    p1.next = p2
+    p2.next = p3
+    return head
 
 def reverseLinkedList_fromMtoN(linkedlist, m, n):
-    return
+    if n < m:
+        return linkedlist
+    index = 0
+    curr = linkedlist
+    while curr:
+        index += 1
+        if index == m - 1:
+            left = curr
+        if index == n:
+            right = curr.next
+            break
+        curr = curr.next
+    start = left.next
+    end = curr
+    left.next = None
+    end.next = None
+    curr = start
+    prev = None
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    start.next = right
+    left.next = prev
+
+    return linkedlist
 
 def demo():
     arr = [1, 2, 3, 4, 5]
