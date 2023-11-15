f2f716fd019d61ad23e762f9fe8293535318439d
youthriver <664163046@qq.com>
2023-11-15 17:13:00 +0800

    update

---
+++ python/binaryTree/inorder.py
@@ -0,0 +1,21 @@
+
+import logging
+import os
+
+logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s', level=logging.DEBUG)
+
+
+# 给定一个二叉树的根节点root, 返回它的中序遍历结果
+# 数据范围: 树上节点数满足0<=n<=1000, 树上每个节点的值满足 -1000 <= val <= 1000
+# 进阶: 空间复杂度O(n), 时间复杂度O(n)
+# 备注: 树中节点数目在范围[0, 100]内树中的节点的值在[-100, 100]以内
+# 示例1: 输入{1, 2, #, #, 3}, 返回值[2, 3, 1]
+# 示例2: 输入{}, 返回值[]
+# 示例3: 输入{1, 2}, 返回值[2, 1]
+# 示例4: 输入{1, #, 2}, 返回值[1, 2]
+
+def demo():
+    pass
+
+if __name__ == '__main__':
+    demo()
