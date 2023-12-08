40c5d19b71f4c5d9c6c02eb2fc154f9b2d48dae1
youthriver <664163046@qq.com>
2023-12-08 15:37:38 +0800

    update

---
+++ python/simulate/rotateMatrix.py
@@ -0,0 +1,17 @@
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(filename)s[%(lineno)d] - %(levelname)s:[%(message)s]', level=logging.DEBUG)
+
+# 顺时针旋转矩阵  中等
+# 有一个NxN整数矩阵, 请编写一个算法, 将矩阵顺时针旋转90度.
+# 给定一个NxN的矩阵, 和矩阵的阶数N, 请返回旋转后的NxN矩阵.
+# 数据范围: 0<n<300, 矩阵中的值满足0<=val<=1000
+# 要求: 空间复杂度O(N^2), 时间复杂度O(N^2)
+# 进阶: 空间复杂度O(1), 时间复杂度O(N^2)
+# 示例一: 输入 [[1,2,3],[4,5,6],[7,8,9]],3  返回值[[7,4,1],[8,5,2],[9,6,3]]
+
+def demo():
+    arr = []
+
+if __name__ == '__main__':
+    demo()
---
+++ python/two_pointer/mergeIntervals.py
@@ -0,0 +1,18 @@
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(filename)s[%(lineno)d] - %(levelname)s:%(message)s', level=logging.DEBUG)
+
+# 合并区间  中等
+# 给出一组区间, 请合并所有重叠的区间.
+# 请保证合并后的区间按区间起点升序排列.
+# 数据范围: 区间组数0<=n<=2*10^5, 区间内的值都满足0<=val<=2*10^5
+# 要求: 空间复杂度O(n), 时间复杂度O(nlogn)
+# 进阶: 空间复杂度O(val), 时间复杂度O(val)
+# 示例一: 输入[[10,30],[20,60],[80,100],[150,180]], 返回值[[10,60],[80,100],[150,180]]
+# 示例二: 输入[[0,10],[10,20]], 返回值[[0,20]]
+
+def demo():
+    arr = []
+
+if __name__ == '__main__':
+    demo()
