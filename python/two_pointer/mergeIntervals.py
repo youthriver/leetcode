6b2541c98980a2d98a890b8a721d2a3df58d63e1
youthriver <664163046@qq.com>
2023-12-11 10:45:38 +0800

    update

--- python/two_pointer/mergeIntervals.py
+++ python/two_pointer/mergeIntervals.py
@@ -11,8 +11,25 @@ logging.basicConfig(format='%(asctime)s - %(filename)s[%(lineno)d] - %(levelname
 # 示例一: 输入[[10,30],[20,60],[80,100],[150,180]], 返回值[[10,60],[80,100],[150,180]]
 # 示例二: 输入[[0,10],[10,20]], 返回值[[0,20]]
 
+def merge_intervals(arr):
+    result = []
+    arr.sort(key=lambda item: item[0])
+    # num = len(arr)
+    for index, item in enumerate(arr):
+        if index == 0:
+            result.append(item)
+        else:
+            if item[0] <= result[-1][1]:
+                result[-1][1] = item[1]
+            else:
+                result.append(item)
+    return result
+
 def demo():
-    arr = []
+    arr = [[10,30],[20,60],[80,100],[150,180]]
+    arr = [[0,10],[10,20]]
+    result = merge_intervals(arr)
+    logging.info(f'result is {result}')
 
 if __name__ == '__main__':
     demo()
