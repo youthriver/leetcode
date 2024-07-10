a15ceb66a1566f1b9e340bd908507bc571f3b7fb
“664163046@qq.com” <664163046sd>
2024-07-10 22:51:51 +0800

    update

---
+++ python/binaryTree/maxDepth.py
@@ -0,0 +1,20 @@
+
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s:%(message)s', level=logging.DEBUG)
+
+# 二叉树的最大深度 简单
+# 求给定二叉树的最大深度，深度是指树的根结点到任一叶子结点路径上节点的数量。最大深度是所有叶子结点的深度的最大值。（注：叶子结点是指没有子节点的节点。）
+# 数据范围：0<=n<=100000，树上每个节点的val满足|val|<=100
+# 要求：空间复杂度O(1)，时间复杂度O(1)
+# 示例一：输入：{1, 2}，返回值：2
+# 示例二：输入：{1, 2, 3, 4, #, #, 5}，返回值：3
+
+def maxDepth(root):
+    return
+
+def demo():
+    arr = []
+
+if __name__ == '__main__':
+    demo()
