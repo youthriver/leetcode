3b3d432fa48af3a8f1636cfc06ee6e0027dd3db1
youthriver <664163046@qq.com>
2024-07-16 18:52:18 +0800

    update

--- python/recursive_backtrack/permutationOfString.py
+++ python/recursive_backtrack/permutationOfString.py
@@ -13,18 +13,37 @@ logging.basicConfig(format='%(asctime)s - %(filename)s[%(lineno)d] - %(levelname
 # 示例3：输入："abc"，返回值：["abc","acb","bac","bca","cab","cba"]
 # 示例4：输入：""，返回值：[""]
 
+# 方法一：从前往后进行全排列，假设只有第一位的情况下，结果为当前字符；对于n位情况，所有排列可能为将第n位字符分别插入n-1位全排列结果中的各个位置,
+# 即假设n-1=2位结果为['ab', 'ba']，则n=3位全排列结果为将第n位元素'c'分别插入'ab'和'ba', 其中插入位置为0->(n-1),即'c'插入'ab'的第0位为'cab',
+# 'c'插入'ab'的第1位为'acb', 'c'插入'ab'的第2位为'abc',同理'c'插入'ba'的第0位为'cba', 'c'插入'ba'的第1位为'bca','c'插入'ba'的第2位为'bac',
+# 对于重复元素的问题需要考虑当前插入的元素与当前插入位置的元素是否相同，如果相同则跳过
+# 方法二：考虑深度优先搜索所有排列方案，即通过字符交换，先固定第1位的字符(n种情况)，再固定第2位字符(n-1种情况)，...，最后固定最后一位字符(1种情况)
+# 当字符串存在重复字符时，排列方案中也存在重复的排列方案。为了排除重复方案，需在固定某位字符时，保证“每种字符只在此位固定一次”，即遇到重复字符时不交换，
+# 直接跳过。从DFS角度看，此操作称为”剪枝“
+
 def permutationOfString(str):
     n = len(str)
+    str = ('').join(sorted(str))
     result = []
-    if n == 0:
-        result = []
-    else:
-        pass
+    for index in range(n):
+        if index == 0:
+            result = [str[index]]
+        else:
+            temp = result
+            result = []
+            for item in temp:
+                for i in range(index + 1):
+                    # 剪枝
+                    if (i >= len(item)) or (str[index] != item[i]):
+                        result.append(item[:i] + str[index] + item[i:])
 
     return result
 
 def demo():
     str = 'ab'
+    # str = ''
+    # str = 'abc'
+    # str = 'aab'
     result = permutationOfString(str)
     logging.info(f'result is {result}')
 
