3fcfb0ce322ad51e6575830df08ed21609cad57c
“664163046@qq.com” <664163046sd>
2024-07-28 11:04:06 +0800

    update

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 链表中倒数最后k个节点 - FindKthToTail 简单
 两个链表的第一个公共节点 - FindFirstCommonNode 简单
 链表相加(二) - addInList 中等
-单链表的排序 - 中等
+单链表的排序 - sortInList 中等
 判断一个链表是否为回文结构 - 简单
 链表的奇偶重排 - 中等
 删除有序链表中重复的元素-I - 简单
--- python/linkedList/sortInList.py
+++ python/linkedList/sortInList.py
@@ -9,8 +9,75 @@ logging.basicConfig(format='%(asctime)s - %(filename)s[%(lineno)d] - %(levelname
 # 示例1: 输入：[1,3,2,4,5], 返回值：{1,2,3,4,5}
 # 示例2: 输入：[-1,0,-2], 返回值：{-2,-1,0}
 
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+def list2linkedlist(arr):
+    head = LinkedList(-1)
+    curr = head
+    for item in arr:
+        curr.next = LinkedList(item)
+        curr = curr.next
+    return head.next
+
+def linkedlist2list(head):
+    result = []
+    while head:
+        result.append(head.value)
+        head = head.next
+    return result
+
+def get_mid(head):
+    slow = head
+    fast = head
+    while fast and fast.next:
+        temp = slow
+        slow = slow.next
+        fast = fast.next.next
+    return temp
+def sortInList(head):
+    if (not head) or (not head.next):
+        return head
+    mid = get_mid(head)
+    right = mid.next
+    left = mid
+    left.next = None
+    left_linkedlist = sortInList(head)
+    right_linkedlist = sortInList(right)
+    result = merge(left_linkedlist, right_linkedlist)
+    return result
+
+def merge(left, right):
+    head = LinkedList(-1)
+    curr = head
+    while left and right:
+        if (left.value <= right.value):
+            curr.next = left
+            curr = curr.next
+            left = left.next
+        else:
+            curr.next = right
+            curr = curr.next
+            right = right.next
+    while left:
+        curr.next = left
+        curr = curr.next
+        left = left.next
+    while right:
+        curr.next = right
+        curr = curr.next
+        right = right.next
+    return head.next
+
 def demo():
-    arr = []
+    arr = [1, 3, 2, 4, 5]
+    arr = [-1, 0, 2]
+    head = list2linkedlist(arr)
+    node = sortInList(head)
+    result = linkedlist2list(node)
+    logging.info(f'result is {result}')
+
 
 if __name__ == '__main__':
     demo()
