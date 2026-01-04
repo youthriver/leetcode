454c7cfce53b3e0ff5743b8bb2dff6fa2e48a2e7
“664163046@qq.com” <664163046sd>
2026-01-04 23:52:35 +0800

    update

--- python/binaryTree/mirror.py
+++ python/binaryTree/mirror.py
@@ -11,8 +11,70 @@ logging.basicConfig(format='%(asctime)s - %(filename)s[%(lineno)d] - %(levelname
 # 示例2: 输入：{}, 返回值：{}
 
 
-def demo():
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def list2tree(arr):
+    num = len(arr)
+    if num < 1:
+        return None
+    head = TreeNode(arr[0])
+    curr = [head]
+    index = 1
+    while (index < num):
+        temp = []
+        for item in curr:
+            if (index < num) and (arr[index] != '#'):
+                item.left = TreeNode(arr[index])
+                temp.append(item.left)
+            index += 1
+            if (index < num) and (arr[index] != '#'):
+                item.right = TreeNode(arr[index])
+                temp.append(item.right)
+            index += 1
+        curr = temp
+    return head
+
+def tree2list(head):
     arr = []
+    curr = [head]
+    while curr:
+        temp = []
+        for item in curr:
+            if item:
+                arr.append(item.value)
+                temp.append(item.left)
+                temp.append(item.right)
+            else:
+                arr.append('#')
+        curr = temp
+    while (arr) and (arr[-1] == '#'):
+        arr.pop()
+    return arr
+
+def mirror(head):
+    if not head:
+        return None
+    if head.left or head.right:
+        temp = head.left
+        head.left = head.right
+        head.right = temp
+    if head.left:
+        mirror(head.left)
+    if head.right:
+        mirror(head.right)
+    return head
+
+def demo():
+    arr = [8, 6, 10, 5, 7, 9, 11]
+    # arr = []
+    head = list2tree(arr)
+    new_head = mirror(head)
+    result = tree2list(new_head)
+    logging.info(f'result is {result}')
 
 if __name__ == '__main__':
     demo()
